2ba9f63cd1278242b7e8c7e2d4416a88c20f9a5f
gregwhitaker <gwhitake@gmail.com>
2017-06-21 14:38:29 -0700

    Doing work

--- src/main/java/com/github/gregwhitaker/ratpackwebsockets/example/ChatHandler.java
+++ src/main/java/com/github/gregwhitaker/ratpackwebsockets/example/ChatHandler.java
@@ -1,6 +1,8 @@
 package com.github.gregwhitaker.ratpackwebsockets.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
 import ratpack.websocket.WebSocket;
@@ -17,41 +19,47 @@ import java.util.Map;
 import java.util.Set;
 
 public class ChatHandler implements Handler {
+    private static final Logger LOG = LoggerFactory.getLogger(ChatHandler.class);
 
     private final ObjectMapper mapper = new ObjectMapper();
-    private final Set<String> users = new HashSet<>();
+    private final Set<String> clients = new HashSet<>();
     private final PublishSubject<String> events = PublishSubject.create();
 
     @Override
     public void handle(Context ctx) throws Exception {
         WebSockets.websocket(ctx, new WebSocketHandler<String>() {
 
+            private WebSocket ws;
+
             @Override
             public String onOpen(WebSocket webSocket) throws Exception {
                 String client = ctx.getRequest().getQueryParams().get("client");
-                Map<String, String> response = new HashMap<>();
 
-                if (client == null) {
-                    // Client id is required
-                    response.put("type", "error");
-                    response.put("message", "Client id is required!");
-                } else if (users.contains(client)) {
-                    // Client can only connect once
-                    response.put("type", "error");
-                    response.put("message", "User is already connected!");
+                LOG.info("Websocket opened for client: {}", client);
+
+                if (client == null || client.isEmpty()) {
+                    webSocket.close(500, "Client id is required");
+                } else if (clients.contains(client)) {
+                    webSocket.close(500, "Client is already connected");
                 } else {
-                    // Client successfully connected
+                    ws = webSocket;
+                    clients.add(client);
+
+                    Map<String, String> response = new HashMap<>();
                     response.put("type", "clientconnect");
                     response.put("client", client);
                     response.put("success", "true");
+
+                    webSocket.send(mapper.writer().writeValueAsString(response));
                 }
 
-                return mapper.writer().writeValueAsString(response);
+                return null;
             }
 
             @Override
             public void onClose(WebSocketClose<String> close) throws Exception {
-                // Noop
+                String client = ctx.getRequest().getQueryParams().get("client");
+                clients.remove(client);
             }
 
             @Override
